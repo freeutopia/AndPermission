7edf7959fcbbd99c90fe99f0cc0616067c086cbd
刘涛 <free_utopia@163.com>
2021-03-15 18:04:33 +0800

    新增开源协议

--- sample/src/main/java/com/utopia/demo/MainActivity.java
+++ sample/src/main/java/com/utopia/demo/MainActivity.java
@@ -3,6 +3,7 @@ package com.utopia.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -15,6 +16,7 @@ import com.utopia.permission.annotation.OnPermissionDenied;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private final static int WRITE_EXTERNAL_STORAGE_CODE = 0X0001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,33 +24,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @NeedsPermission(value = Manifest.permission.WRITE_EXTERNAL_STORAGE,requestCode = 0)
+    @NeedsPermission(value = Manifest.permission.WRITE_EXTERNAL_STORAGE,requestCode = WRITE_EXTERNAL_STORAGE_CODE)
     public void getAllSdcardFiles(View view) {
-        File rootFile = Environment.getStorageDirectory();
-        Log.e("test","rootFile:"+rootFile.getAbsolutePath());
-
-        File[] listFiles = rootFile.listFiles();
-        Log.e("test","listFiles:"+listFiles.length);
+        Log.e("test","我读写了SD卡");
     }
 
-    @OnPermissionDenied
-    public void onPermissionDenied(){
-        Log.e("test","权限被拒绝了");
-    }
-
+    /**
+     * 也可以是无参方法，建议使用此回调
+     */
     @OnPermissionDenied
     public void onPermissionDenied(int requestCode){
-        Log.e("test","权限被拒绝了:"+requestCode);
+        if (WRITE_EXTERNAL_STORAGE_CODE == requestCode) {
+            Log.e("test", "Manifest.permission.WRITE_EXTERNAL_STORAGE 权限被拒绝了");
+        }
     }
 
     @OnNeverAskAgain
     public void onNeverAskAgain(int requestCode){
         Log.e("test","不要在询问了:"+requestCode);
     }
-
-    @OnNeverAskAgain
-    public void onNeverAskAgain(){
-        Log.e("test","不要在询问了");
-    }
-    
 }
